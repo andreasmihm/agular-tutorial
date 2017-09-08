b189ac4b1772c5f3a8c861e34cff7f26ffbd63af
Andreas Mihm <andreas.mihm@gmx.de>
2017-09-08 11:32:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ angular.module('tutorialApp', [])
       { id: 1, name: "Pizza Vegetaria", price: 5 },
       { id: 2, name: "Pizza Salami",    price: 5.5 },
       { id: 2, name: "Pizza Funghi",    price: 5.5 },
+      { id: 2, name: "Pizza Quattro stagioni",    price: 5.5 },
       { id: 3, name: "Pizza Thunfisch", price: 6 }
     ];
   });
